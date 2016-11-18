ba793b7f532bb14035703e4bad3f2e4a9e17cd99
Locke <moyerock@gmail.com>
2016-11-19 05:32:51 +0800

    feat: all in

--- bin/app.js
+++ bin/app.js
@@ -10,61 +10,61 @@ nightmare
   .type('#password', config.site.password)
   .click('img[src*="images/login2/dl_1.jpg"]')
   .wait('#mainPanel')
-  .evaluate(() => {
+  //.evaluate(() => {
     /* eslint-disable */
-    Date.prototype.Format = function (fmt) { //author: meizz
-      var o = {
-        "M+": this.getMonth() + 1, //月份
-        "d+": this.getDate(), //日
-        "H+": this.getHours(), //小时
-        "m+": this.getMinutes(), //分
-        "s+": this.getSeconds(), //秒
-        "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-        "S": this.getMilliseconds() //毫秒
-      };
-      if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-      for (var k in o)
-        if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-      return fmt;
-    };
-    function findButtonbyTextContent(text) {
-      var buttons = document.querySelectorAll('button');
-      for (var i=0, l=buttons.length; i<l; i++) {
-        if (buttons[i].firstChild.nodeValue == text)
-          return buttons[i];
-      }
-    }
-    function randText() {
-      return Math.random().toString(36).substr(2);
-    }
-    function logWithTime(text) {
-      console.log(new Date().Format("yyyy-MM-dd HH:mm:ss"), text);
-    }
-    logWithTime('处理开始');
-    var southPanel = view.findById('southPanel');
-
-    function handle() {
-      var win = document.querySelector('#downAnsWindow');
-      if (win && win.style.visibility === 'visible') {
-        logWithTime('应答对话框出现')
-        // 选择问题：
-        southPanel.downAnsListGrid.getSelectionModel().selectFirstRow();
-        // 点击回答问题按钮：
-        findButtonbyTextContent('回答问题').click();
-        // 填答案:
-        var answer = randText();
-        document.querySelector('textarea[name="answer"]').value = answer;
-        // 点击确定按钮:
-        findButtonbyTextContent('确定').click();
-        // 关闭窗口：
-        setTimeout(() => {
-          logWithTime('答应消息:' + answer)
-          Ext.WindowMgr.getActive().close();
-        }, 1000);
-      }
-    }
-
-    setInterval(handle, 2000);
+    //Date.prototype.Format = function (fmt) { //author: meizz
+    //  var o = {
+    //    "M+": this.getMonth() + 1, //月份
+    //    "d+": this.getDate(), //日
+    //    "H+": this.getHours(), //小时
+    //    "m+": this.getMinutes(), //分
+    //    "s+": this.getSeconds(), //秒
+    //    "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+    //    "S": this.getMilliseconds() //毫秒
+    //  };
+    //  if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    //  for (var k in o)
+    //    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    //  return fmt;
+    //};
+    //function findButtonbyTextContent(text) {
+    //  var buttons = document.querySelectorAll('button');
+    //  for (var i=0, l=buttons.length; i<l; i++) {
+    //    if (buttons[i].firstChild.nodeValue == text)
+    //      return buttons[i];
+    //  }
+    //}
+    //function randText() {
+    //  return Math.random().toString(36).substr(2);
+    //}
+    //function logWithTime(text) {
+    //  console.log(new Date().Format("yyyy-MM-dd HH:mm:ss"), text);
+    //}
+    //logWithTime('处理开始');
+    //var southPanel = view.findById('southPanel');
+    //
+    //function handle() {
+    //  var win = document.querySelector('#downAnsWindow');
+    //  if (win && win.style.visibility === 'visible') {
+    //    logWithTime('应答对话框出现')
+    //    // 选择问题：
+    //    southPanel.downAnsListGrid.getSelectionModel().selectFirstRow();
+    //    // 点击回答问题按钮：
+    //    findButtonbyTextContent('回答问题').click();
+    //    // 填答案:
+    //    var answer = randText();
+    //    document.querySelector('textarea[name="answer"]').value = answer;
+    //    // 点击确定按钮:
+    //    findButtonbyTextContent('确定').click();
+    //    // 关闭窗口：
+    //    setTimeout(() => {
+    //      logWithTime('答应消息:' + answer)
+    //      Ext.WindowMgr.getActive().close();
+    //    }, 1000);
+    //  }
+    //}
+    //
+    //setInterval(handle, 2000);
 
     //var southPanel = view.findById('southPanel');
     //southPanel.datas['downAns'].push({
@@ -94,19 +94,81 @@ nightmare
 
     // return document.title;
     /* eslint-enable */
-  })
+  //})
+  .evaluate(() => document.title)
   .then(title => print(`${title} => 加载完成`))
   .catch(err => error(err))
 
-nightmare.on('page', (type, ...args) => {
-  if (type === 'error') {
-    error(args)
-  } else if (type === 'alert') {
-    print('alert')
-  } else if (type === 'prompt') {
-    print('prompt', args)
-  } else if (type === 'confirm') {
-    print('confirm', args)
-  }
-})
+nightmare
+  .on('page', (type, ...args) => {
+    if (type === 'error') {
+      error(args)
+    } else if (type === 'alert') {
+      print('alert')
+    } else if (type === 'prompt') {
+      print('prompt', args)
+    } else if (type === 'confirm') {
+      print('confirm', args)
+    }
+  })
 
+async function checkWindow() {
+  printWithTime('起...')
+  const visible = await nightmare.visible('#downAnsWindow')
+  if (visible) {
+    printWithTime('弹窗出现')
+
+    await nightmare.evaluate(() => {
+      /* eslint-disable */
+      Date.prototype.Format = function (fmt) { //author: meizz
+        var o = {
+          "M+": this.getMonth() + 1, //月份
+          "d+": this.getDate(), //日
+          "H+": this.getHours(), //小时
+          "m+": this.getMinutes(), //分
+          "s+": this.getSeconds(), //秒
+          "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+          "S": this.getMilliseconds() //毫秒
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+          if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+      };
+      function findButtonbyTextContent(text) {
+        var buttons = document.querySelectorAll('button');
+        for (var i=0, l=buttons.length; i<l; i++) {
+          if (buttons[i].firstChild.nodeValue == text)
+            return buttons[i];
+        }
+      }
+      function randText() {
+        return Math.random().toString(36).substr(2);
+      }
+      function logWithTime(text) {
+        console.log(new Date().Format("yyyy-MM-dd HH:mm:ss"), text);
+      }
+      logWithTime('处理开始');
+      var southPanel = view.findById('southPanel');
+      // 选择问题：
+      southPanel.downAnsListGrid.getSelectionModel().selectFirstRow();
+      // 点击回答问题按钮：
+      findButtonbyTextContent('回答问题').click();
+      // 填答案:
+      var answer = randText();
+      document.querySelector('textarea[name="answer"]').value = answer;
+      // 点击确定按钮:
+      findButtonbyTextContent('确定').click();
+      // 关闭窗口：
+      setTimeout(() => {
+        logWithTime('答应消息:' + answer)
+        Ext.WindowMgr.getActive().close();
+      }, 1000);
+      /* eslint-enable */
+    })
+
+    setTimeout(checkWindow, 5000)
+  }
+}
+
+setTimeout(checkWindow, 5000)
--- config/index.js
+++ config/index.js
@@ -5,7 +5,9 @@ global.env = process.env.NODE_ENV || 'development'
 global.fs = require('fs')
 global.path = require('path')
 global.debug = require('debug')
+global.moment = require('moment')
 global.debugInfo = debug('app')
+global.printWithTime = (text) => print(moment().format('YYYY-MM-DD HH:mm:ss'), text)
 
 global.config = require(`./${env}`)
 try {
